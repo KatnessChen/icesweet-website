bb460ec15f4a558eb277431164468f264ef7f14c
KC <sbxew99@gmail.com>
2021-10-22 16:11:49 +0800

    feat: update Video link

--- components/home/SectionVideo.vue
+++ components/home/SectionVideo.vue
@@ -4,7 +4,7 @@
       class="video"
       width="560"
       height="315"
-      src="https://www.youtube.com/embed/DWcJFNfaw9c"
+      src="https://www.youtube.com/watch?v=dRCxY7FRwbw"
       title="YouTube video player"
       frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
