d3adb0ab477509073edf82dfaefc5ce5ec080d81
KC <sbxew99@gmail.com>
2021-10-29 23:10:45 +0800

    fix: content

--- components/home/SectionSwiper.vue
+++ components/home/SectionSwiper.vue
@@ -29,14 +29,14 @@ export default {
     banners () {
       return this.isMobile
         ? [
-            'image-sm-1.webp',
             'image-sm-2.webp',
-            'image-sm-3.webp'
+            'image-sm-3.webp',
+            'image-sm-1.webp'
           ]
         : [
-            'image-1.webp',
             'image-2.webp',
-            'image-3.webp'
+            'image-3.webp',
+            'image-1.webp'
           ]
     },
     styleInfo () {
